8546e345b46cd6761b24ea68c7a5359aba011ded
Joshua Woods <nordev@protonmail.com>
2023-11-20 13:35:28 -0600

    added go server

---
+++ goserver/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    // Register the homeHandler function for the root route
+    http.HandleFunc("/", homeHandler)
+
+    // Start the server on port 8080
+    log.Println("Server starting on :8080...")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    // Send a response back to the client
+    w.Write([]byte("hello from go server"))
+}
